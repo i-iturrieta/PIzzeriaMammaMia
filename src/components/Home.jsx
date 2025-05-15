8185af8c5a3c7d8e837f135703f630e20408d4cb
Ignacio <nachito.iturrieta@gmail.com>
2025-05-14 23:51:10 -0400

    Cambio de imagen

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,8 @@ const Home = () => {
                     name="Napolitana"
                     price={5000}
                     ingredients={['queso', 'tomate', 'aceitunas']}
-                    img='public\img\pizzaImg.jpeg'
+                    img="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"
+
                 /> 
 
                 <CardPizza
