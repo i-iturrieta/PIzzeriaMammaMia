bf650568480bd685ce29f82c841974e94a5983ec
Ignacio <nachito.iturrieta@gmail.com>
2025-05-25 19:34:37 -0400

    hito 3, revisado

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 //import RegisterPage from './components/RegisterPage';
 //import LoginPage from './components/LoginPage';
-import Cart from './components/cart';
+import Cart from './components/Cart';
 import './App.css';
 function App() {
   return (
